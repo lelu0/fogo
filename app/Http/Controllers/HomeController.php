53d7901fe48bd4ba4f6934c1dba249bf02e636f2
lelu0 <kamil@lznets.pl>
2020-05-08 12:14:58 +0000

    next_race selection fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     public function index(){
         $results = Result::orderBy('id', 'DESC')->take(10)->get();
         $leagues = League::where('active', 1)->orderBy('id', 'DESC')->take(5)->get();
-        $nextRace = NextRace::whereDate('date', '>', Carbon::now())->orderBy('date', 'ASC')->first();
+        $nextRace = NextRace::whereTime('date', '>=', Carbon::now())->orderBy('date', 'ASC')->first();
         $contact = Contact::all();
         $drivers = Driver::where('active', 1)->get();
         $partners = Partner::where('active', 1)->get();
